511db084585630aaf2dabe2735ee6600832c63c0
ahetric <55210657+ahetric@users.noreply.github.com>
2019-12-10 16:16:49 -0500

    updates to main + data

--- political.py
+++ political.py
@@ -1,8 +1,14 @@
 '''
-put our info here
+James Edwards
+Amelia Hetrick
+
+Created: November 2019
+Updated: December 10 2019
 
 Example run:
-python3 political.py primary_wo_noise.csv general_wo_noise.csv out.dat
+python3 political.py primary_stemmed.csv primaryClassified.dat general_stemmed.csv generalClassified.dat out.dat
+
+Majority of code outline taken from:
 
 '''
 
@@ -14,6 +20,8 @@ from sklearn.metrics.pairwise import cosine_similarity         # cosine similari
 from scipy.sparse import csr_matrix                            # sparse matrix
 from collections import Counter                                # list counter (most common element)
 import csv
+from collections import defaultdict
+import numpy as np
 
 #imports to tokenize/stem
 from nltk import RegexpTokenizer
@@ -21,12 +29,14 @@ from nltk.stem.porter import PorterStemmer
 import re
 
 '''
-Step 0. Read command line arguments.
+Read command line arguments.
 Looking for:
-  0) file name
-  1) train file
-  2) test file
-  3) output file (test file's classification labels)
+  0) input  file  name
+  1) input  train data   file
+  2) input  train labels file
+  3) input  test  data   file
+  4) input  test  labels file (ground truth)
+  5) output test  labels file (predicted)
 '''
 
 trainFile = ''
@@ -44,7 +54,7 @@ if (len(sys.argv) == 6):
     outputFile = sys.argv[5]
 else:
     print('Please enter acceptable command line arguments:')
-    print('\t[program] [train in] [train labels] [test in] [test labels] [predicted labels out]')
+    print('\t[program] [train in] [train labels in] [test in] [test labels in] [predicted labels out]')
     sys.exit(0)
 
 # print confirmations of command line inputs
@@ -58,46 +68,54 @@ print('')
 print('Working...')
 print()
 
-#open training file
-with open(trainFile, 'r'):
-    print("yay")
 
-#Code to read training file,
+train= []
+trainLabels = []
+test = []
+testLables = []
+output = []
 
+# TO DO : REMOVE FIRST ROW OF LABLES IN THIS SECTION
 
+#open train file
+with open(trainFile, 'r') as file:
+    csv1 = csv.reader(file, delimiter=',')
+    columns = defaultdict(list)
+    for row in csv1:
+        for (i,v) in enumerate(row):
+            columns[i].append(v)
+    train = columns[2]
+print(train[0])
+train = np.array(train)
+print(type(train))
 
-
-#testfile = open(test, 'r')
-
-'''
-Step 1. 
-'''
-
-
-
-print('Step 1 of 3. Still working...')
-print()
-
-
-
-'''
-Step 2. 
-'''
-
+#open train labels file
+with open(trainLabelsFile, 'r') as file:
+    trainLabels = file.readlines()
 
 
-print('Step 2 of 3. Still working...')
-print()
+#open test file
+with open(testFile, 'r') as file:
+    csv2 = csv.reader(file, delimiter=',')
+    columns = defaultdict(list)
+    for row in csv2:
+        for (i,v) in enumerate(row):
+            columns[i].append(v)
+    test = columns[2]
+print(test[1])
+test = np.array(test)
+print(type(test))
+
+
+#open test labels file
+with open(testLabelsFile, 'r') as file:
+    testLabels = file.readlines()
 
 
 
-'''
-Step 3. 
-'''
 
-
-
-print('Step 3 of 3. Program run done! See output file.')
+print(train.shape)
+#sys.exit(0)
 
 
 
@@ -108,14 +126,23 @@ keras
 
 
 '''
+#https://stackabuse.com/time-series-analysis-with-lstm-using-pythons-keras-library/
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Embedding
 from keras.layers import LSTM
 from keras.layers import Dropout
 
 model = Sequential()
 
-model.add(LSTM(units=50, return_sequences=True, input_shape=(features_set.shape[1], 1)))
+
+#model.add(LSTM(units=50, return_sequences=True, input_shape=(train.shape, 1)))
+model.add(LSTM(units=50, return_sequences=True, input_shape=(train.shape)))
+#model.add(LSTM(units=50, return_sequences=True, input_dim=(3)))
+#max_talk_len = 500
+#X_train = sequence.pad_sequence(train, maxlen=max_talk_len)
+
+#model.add(Embedding(500, embedding_vecor_length, input_length=max_talk_len))
+
 
 model.add(Dropout(0.2))
 
@@ -128,12 +155,78 @@ model.add(Dropout(0.2))
 model.add(LSTM(units=50))
 model.add(Dropout(0.2))
 
-model.add(Dense(units = 1))
+model.add(Dense(1, activation='sigmoid'))
 
 model.compile(optimizer = 'adam', loss = 'mean_squared_error')
+#model.compile(loss='binary_crossentropy', optimizer = 'adam')
 
-model.fit(features_set, labels, epochs = 100, batch_size = 32)
+model.fit(train, trainLabels, epochs = 100, batch_size = 32)
+#model.fit(X_train, trainLabels, epochs = 100, batch_size = 32)
 '''
+
+
+#https://www.kaggle.com/kredy10/simple-lstm-for-text-classification
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from keras.models import Model
+from keras.layers import LSTM, Activation, Dense, Dropout, Input, Embedding
+from keras.optimizers import RMSprop
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing import sequence
+from keras.utils import to_categorical
+from keras.callbacks import EarlyStopping
+#matplotlib inline
+
+df = pd.read_csv(trainFile,delimiter=',',encoding='latin-1')
+df.head()
+
+print('\n')
+
+cols = [0,1,3,4,5,6]
+df.drop(df.columns[cols],axis=1,inplace=True)
+df.head()
+df.info()
+
+#Labels = pd.DataFrame({'trainLabels': trainLabels})
+
+#print(df.Text)
+X = df.Text
+Y = trainLabels
+le = LabelEncoder()
+Y = le.fit_transform(Y)
+Y = Y.reshape(-1,1)
+
+X_train,X_test,Y_train,Y_test = train, trainLabels, test, testLabels
+
+max_words = 1000
+max_len = 150
+tok = Tokenizer(num_words=max_words)
+tok.fit_on_texts(X_train)
+sequences = tok.texts_to_sequences(X_train)
+sequences_matrix = sequence.pad_sequences(sequences,maxlen=max_len)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
