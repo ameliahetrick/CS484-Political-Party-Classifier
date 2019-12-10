23350705443e03940c5cae58ec9cfe078790f90f
ahetric <55210657+ahetric@users.noreply.github.com>
2019-12-10 16:19:14 -0500

    more updates pasted

--- political.py
+++ political.py
@@ -210,18 +210,32 @@ sequences = tok.texts_to_sequences(X_train)
 sequences_matrix = sequence.pad_sequences(sequences,maxlen=max_len)
 
 
+def RNN():
+    inputs = Input(name='inputs',shape=[max_len])
+    layer = Embedding(max_words,50,input_length=max_len)(inputs)
+    layer = LSTM(64)(layer)
+    layer = Dense(256,name='FC1')(layer)
+    layer = Activation('relu')(layer)
+    layer = Dropout(0.5)(layer)
+    layer = Dense(1,name='out_layer')(layer)
+    layer = Activation('sigmoid')(layer)
+    model = Model(inputs=inputs,outputs=layer)
+    return model
+
+model = RNN()
+model.summary()
+model.compile(loss='binary_crossentropy',optimizer=RMSprop(),metrics=['accuracy'])
+
+model.fit(sequences_matrix,Y_train,batch_size=128,epochs=10,
+          validation_split=0.2,callbacks=[EarlyStopping(monitor='val_loss',min_delta=0.0001)])
+
+test_sequences = tok.texts_to_sequences(X_test)
+test_sequences_matrix = sequence.pad_sequences(test_sequences,maxlen=max_len)
+
+accr = model.evaluate(test_sequences_matrix,Y_test)
 
 
-
-
-
-
-
-
-
-
-
-
+print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(accr[0],accr[1]))
 
 
 
